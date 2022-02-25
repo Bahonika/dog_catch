f0f3c4ad51b33234070d710ba6f52c6220edbe64
Bahonika <bahonika1@mail.ru>
2022-02-25 20:01:43 +0300

    card_add

---
+++ lib/screens/animal_card_add.dart
@@ -0,0 +1,13 @@
+class AnimalCardAdd extends StatefulWidget {
+  @override
+  _AnimalCardAddState createState() => _AnimalCardAddState();
+}
+
+class _AnimalCardAddState extends State<AnimalCardAdd> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
